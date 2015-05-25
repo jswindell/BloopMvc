4f5d0beaad0274e19102bf3636d044f55370aeae
Hiro <tamokuteki@gmail.com>
2015-05-25 15:41:00 -0400

    Model implementation without Database persistence.
    No data access object/layer is implemented for simplification.
    Model data exist only in memory.

---
+++ BloopMvc/BloopMvc/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BloopMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloopMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            List<BloopFile> models = new List<BloopFile>();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "App_Data";
+            
+            if (Directory.Exists(path))
+            {
+                String[] files = Directory.GetFiles(path);
+
+                foreach (String file in files)
+                {
+                    if(file.EndsWith(".txt"))
+                    {
+                        var model = new BloopFile();
+
+                        model.Name = file;
+                        model.DateTime = System.IO.File.GetLastWriteTime(file);
+                        model.Content = System.IO.File.ReadAllText(file);
+                        models.Add(model);
+                    }
+                }                 
+            }
+            else
+            {
+                Console.WriteLine("{0} does not exist.", path);
+            }  
+
+            return View(models);
+        }
+    }
+}
---
+++ BloopMvc/BloopMvc/Models/BloopFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BloopMvc.Models
+{
+    public class BloopFile
+    {
+        public String Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public String Content { get; set; }
+    }
+}
