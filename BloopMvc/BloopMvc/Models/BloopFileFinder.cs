a20921e5b3b2a88f4cf281a9a3a00fd8d50bccc9
James Swindell <swintwin@gmail.com>
2015-05-27 16:05:01 -0400

    adds new utility class for file listing

---
+++ BloopMvc/BloopMvc/Models/BloopFileFinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace BloopMvc.Models
+{
+    internal static class BloopFileFinder
+    {
+        public static IEnumerable<BloopFile> FindFiles(string path)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+
+            if (!Directory.Exists(path))
+                return Enumerable.Empty<BloopFile>();
+
+            int fileId = 0;
+            return Directory.GetFiles(path)
+                .Where(file => file.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                .Select(file => new BloopFile
+                {
+                    Id = ++fileId,
+                    Name = Path.GetFileName(file),
+                    DateTime = File.GetLastWriteTime(file),
+                    Content = File.ReadAllText(file)
+                });
+        }
+    }
+}
